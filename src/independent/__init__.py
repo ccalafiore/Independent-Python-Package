ab5a21ef55174f5a0ad03f45ed233720ac56f9ec
Carmelo Calafiore <dr.carmelo.calafiore@gmail.com>
2026-09-24 13:46:19 +0100

    Update __init__.py

--- src/independent/__init__.py
+++ src/independent/__init__.py
@@ -1,6 +1,21 @@
 
-
+import datetime
 from . import sequence as seq
+
 
 __all__ = ["seq"]
 
+__version__ = "0.0.0.1"
+
+# TODO: Get the release timestamp automatically from PyPI???
+__release_day__ = 24
+__release_month_num__ = 9
+__release_year__ = 2026
+
+__release_date_object__ = datetime.date(__release_year__, __release_month_num__, __release_day__)
+__release_date__ = __release_date_object__.__format__('%d %B %Y')
+__release_month_name__ = __release_date_object__.__format__('%B')
+
+__author__ = 'Calafiore Carmelo'
+__author_email__ = 'dr.carmelo.calafiore@gmail.com'
+__maintainer_email__ = 'dr.carmelo.calafiore@gmail.com'
